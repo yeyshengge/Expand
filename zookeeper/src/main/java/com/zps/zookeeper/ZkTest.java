30698da8a8803c37a9130051129fdddb072e4167
zps <2235969352@qq.com>
2020-09-01 22:01:21 +0800

    新增代码

---
+++ zookeeper/src/main/java/com/zps/zookeeper/ZkTest.java
@@ -0,0 +1,46 @@
+package com.zps.zookeeper;
+
+import org.apache.zookeeper.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by zps on 2020/8/30 18:10
+ */
+public class ZkTest {
+    private static ZooKeeper zkClient;
+
+    @Before
+    public void init() throws IOException {
+        zkClient = new ZooKeeper("120.27.219.6:2181", 300000, new Watcher() {
+            public void process(WatchedEvent event) {
+                try {
+                    List<String> children = zkClient.getChildren("/sangou", true);
+                    for (int i = 0; i < children.size(); i++) {
+                        System.out.println(children);
+                    }
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        System.out.println(zkClient.toString());
+    }
+
+    @Test
+    public void createNode() throws IOException, KeeperException, InterruptedException {
+        String s = zkClient.create("/ss", "louguanzhong".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println(s);
+    }
+
+    @Test
+    public void listener() throws KeeperException, InterruptedException {
+      zkClient.getChildren("/sangou", true);
+      Thread.sleep(Long.MAX_VALUE);
+    }
+}
